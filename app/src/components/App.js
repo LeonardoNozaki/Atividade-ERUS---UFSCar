fdf882b674b25aa9d80ac1f481f163ceb9a1bcab
lauanS <lauan.s.souza@gmail.com>
2019-10-09 17:03:15 -0300

    Adicionando o Footer

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import MyNavbar from './Navbar';
-
+import Footer from './Footer'
 export default class App extends React.Component{
   render(){
     return (
       <div>
         <MyNavbar />
+        <Footer />
       </div>
     );
   }
