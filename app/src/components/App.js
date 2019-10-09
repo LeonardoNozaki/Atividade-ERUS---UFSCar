de4dfa32c8bd4b6ddd9d39fbdf0fffd4a92941ca
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2019-10-09 17:16:59 -0300

    Alteracoes na navbar

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-import MyNavbar from './Navbar';
+import MyNavbar from './Navbar/index';
 import Footer from './Footer'
+
 export default class App extends React.Component{
   render(){
     return (
